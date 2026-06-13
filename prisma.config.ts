93bf189a83b41840a7ee85009c151f566dd88031
Bartholomew Shekari <barthsheyin@gmail.com>
2026-06-13 11:09:29 +0100

    Deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,3 +1,10 @@
-export default {
+import { defineConfig } from '@prisma/internals';
+
+export default defineConfig({
   schema: './idl-ris/prisma/schema.prisma',
-};
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
